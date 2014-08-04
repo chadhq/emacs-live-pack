c781683dd5487426110a95c4bae41088bf01190d
Chad Harris <chadharrishq@gmail.com>
2014-08-04 15:51:40 +1000

    added kibit and emmet zen coding

--- init.el
+++ init.el
@@ -29,10 +29,21 @@
 (define-key clojure-mode-map (kbd "C-c M-j") 'cider-jack-in)
 (define-key clojure-mode-map (kbd "C-c M-J") 'cider-restart)
 
+(define-key paredit-mode-map [s-down] 'paredit-forward-down)
+(define-key paredit-mode-map [s-right] 'paredit-forward-up)
+(define-key paredit-mode-map [s-left] 'paredit-backward-down)
+(define-key paredit-mode-map [s-up] 'paredit-backward-up)
+
+(define-key paredit-mode-map (kbd "s-<") 'backward-sexp)
+(define-key paredit-mode-map (kbd "s->") 'forward-sexp)
+
 (global-set-key (kbd "C-M-<") 'shrink-window-horizontally)
 (global-set-key (kbd "C-M->") 'enlarge-window-horizontally)
 (global-set-key (kbd "C-M-\"") 'shrink-window)
+
 (global-set-key (kbd "C-M-:") 'enlarge-window)
+;(global-set-key (kbd "C-c r m") 'set-rectangular-region-anchor)
+
 
 
 (global-set-key [M-left] 'previous-buffer)
@@ -40,7 +51,11 @@
 
 (define-key clojure-mode-map (kbd "s-i") 'cider-eval-last-sexp)
 (define-key clojure-mode-map (kbd "s-I")  'cider-eval-last-sexp-and-append)
+
 (global-set-key (kbd "C-^") 'top-join-line)
+
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 
 ;; Append result of evaluating previous expression (Clojure):
 (defun cider-eval-last-sexp-and-append ()
